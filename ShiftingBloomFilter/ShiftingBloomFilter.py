5081a609b01020194504c4e11b4de8f66dd60f04
hero24 <hero24@interia.pl>
2018-02-16 14:51:49 +0000

    import hashlib

--- ShiftingBloomFilter/ShiftingBloomFilter.py
+++ ShiftingBloomFilter/ShiftingBloomFilter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import hashlib
 from hashlib import algorithms_guaranteed
 from sys import byteorder
 
@@ -6,9 +7,13 @@ class ShiftingBloomFilter:
     def __init__(self, length,hash_count=len(algorithms_guaranteed)):
         self.m = length
         self.k = hash_count
+        self.koff = self.k/2
         self.hashfunc = [getattr(hashlib,h) for h in algorithms_guaranteed]
         self.hashfunc = self.hashfunc[0:self.k]
         self.filter = bytearray(self.m)
 
     def _get_hash(self,h,s):
         return int.from_bytes(h(s.encode()).digest(), byteorder) % self.m
+
+
+
