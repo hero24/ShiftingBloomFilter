bda0d5afef6813d1404120e5d2b558c8f7259897
hero24 <hol1@csg21-47.ucc.ie>
2018-02-16 14:00:48 +0000

    Constructor for ShiftingBloomFilter class

---
+++ ShiftingBloomFilter/ShiftingBloomFilter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from hashlib import algorithms_guaranteed
+from sys import byteorder
+
+class ShiftingBloomFilter:
+    def __init__(self, length,hash_count=len(algorithms_guaranteed)):
+        self.m = length
+        self.k = hash_count
+        self.hashfunc = [getattr(hashlib,h) for h in algorithms_guaranteed]
+        self.hashfunc = self.hashfunc[0:self.k]
+        self.filter = bytearray(self.m)
+
+    def _get_hash(self,h,s):
+        return int.from_bytes(h(s.encode()).digest(), byteorder) % self.m
---
+++ ShiftingBloomFilter/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name="ShiftingBloomFilter",
+      version="0.01b",
+      description="Implementation of shifting bloom filter data structure")
+
+     
+
