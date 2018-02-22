ab1030d76486a036e8508ad83a24a2508821492c
hero24 <hero24@interia.pl>
2018-02-22 12:27:46 +0000

    Random string generator added to utils

--- ShiftingBloomFilter/Utils.py
+++ ShiftingBloomFilter/Utils.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from random import randint
 
 class CSVDataSet:
     def __init__(self,filename, separator=','):
@@ -18,3 +19,26 @@ class CSVDataSet:
         return self
 
 
+
+class RandomStringGenerator:
+    def __init__(self,string_length=4,ascii_start=32,ascii_end=126,stream_length=...):
+        self.length = string_length
+        self.start = ascii_start
+        self.end = ascii_end
+        self.len = stream_length
+        self.count = 0
+
+    def __len__(self):
+        return self.len
+
+    def __next__(self):
+        if self.len is not ... and self.count > self.len:
+            raise StopIteration
+        s = ""
+        for _ in range(self.length):
+            s += chr(randint(self.start,self.end))
+        self.count += 1
+        return s
+
+    def __iter__(self):
+        return self
