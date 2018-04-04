3e9cb3903aa6f416b9a3eea5d1041c9221acbd2d
hero24 <hero24@interia.pl>
2018-04-04 22:31:31 +0100

    fixing setup.py script

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ from distutils.core import setup
 setup(name="ShiftingBloomFilter",
       version="0.01b",
       description="Implementation of shifting bloom filter data structure",
-      python_requires=">3.0"
+      python_requires=">3.0",
+      packages=["ShiftingBloomFilter"]
       )
 
      
